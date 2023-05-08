e96271168944ce0462a01a9160870e057340c855
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-05-08 14:03:01 +0300

    Save changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>СУка блять палучілося. Небесний йдем ломати кремль</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
